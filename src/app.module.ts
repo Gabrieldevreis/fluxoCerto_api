20367c16a9b502a48f1e1a516f76fa4821b9d559
gabrieldevreis <gabrielreisdev@gmail.com>
2026-01-17 16:54:35 -0300

    feat: configure global settings and import necessary modules in AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,29 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaModule } from './prisma/prisma.module';
+import { UserModule } from './user/user.module';
+import { ClientModule } from './client/client.module';
+import { OrderModule } from './order/order.module';
+import { IncomeModule } from './income/income.module';
+import { ExpenseModule } from './expense/expense.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    PrismaModule,
+    UserModule,
+    ClientModule,
+    OrderModule,
+    IncomeModule,
+    ExpenseModule,
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
