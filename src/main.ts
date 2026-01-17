ce5c9ec660f168fde5654589a1203db378e43822
gabrieldevreis <gabrielreisdev@gmail.com>
2026-01-17 16:56:21 -0300

    feat: Add Swagger configuration to API documentation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
+import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+   // Configuração básica do Swagger
+  const config = new DocumentBuilder()
+    .setTitle('API Fluxo Certo')
+    .setDescription('Sistema de Finanças com NestJS + Prisma')
+    .setVersion('1.0')
+    .addBearerAuth() // JWT
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
